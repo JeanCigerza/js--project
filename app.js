86114ffba82254c28fce7cd346df66b2d434ba43
jeanscigerza@gmail.com <98674135+JeanCigerza@users.noreply.github.com>
2024-01-27 16:18:16 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+alert('Olá mundo');
+let numeroSecreto = 5;
+console.log(numeroSecreto);
+let numeroEscolhido = prompt('Escolha um número entre 1 e 10');
+
+
+if(numeroSecreto =+ numeroEscolhido){
+  console.log('Parabens');
+} else {
+    alert('Você errou');
+}
